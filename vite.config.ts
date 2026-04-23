467f06aabd7864e8f1ad1e25578286f109d6ab04
Ziedblg7 <Zbouallagui999@gmail.com>
2026-04-23 22:00:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
-  base: '/whitecoffee/', // This MUST have the slashes at start and end
+  base: './', // This MUST have the slashes at start and end
   // ... rest of your code
 })
